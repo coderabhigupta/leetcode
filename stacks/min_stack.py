d09cb40a11dee9b45351d520335501bb206ab9e5
coderabhigupta <abhigupta@Abhis-MacBook-Pro-2018.local>
2024-05-06 12:04:36 -0400

    Added stacks

---
+++ stacks/min_stack.py
@@ -0,0 +1,36 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        if self.minStack[-1] == self.stack.pop():
+            self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+ms = MinStack()
+ms.push(-1)
+ms.push(0)
+assert ms.getMin() == -1
+ms.push(-2)
+assert ms.top() == -2
+ms.push(1)
+assert ms.getMin() == -2
+ms.push(-3)
+assert ms.getMin() == -3
+assert ms.top() == -3
+ms.pop()
+assert ms.getMin() == -2
